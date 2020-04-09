8f0e473d1843d44d47e7a8d67654f77af71016fb
Andre <andre@verhoek.nl>
2020-04-09 20:10:42 +0200

    Loaded in data

--- Titanic_forecast.py
+++ Titanic_forecast.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+# Common imports
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+
 df = pd.read_csv('Data/train.csv',index_col = 'PassengerId')
-print(df)
+print(list(df.columns.values))
+
+X = df[['Sex','Age','SibSp','Parch','Embarked']]
+y = df[['Survived']]
+
+tree_clf_entropy = DecisionTreeClassifier(max_depth=2, random_state=42, criterion='entropy')
+tree_clf_entropy.fit(X,y)
